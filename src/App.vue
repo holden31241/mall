a41a804ac7f0d20237469fc99c9da902d0929f10
holden <zcy19998006@163.com>
2023-06-27 21:08:59 +0800

    跨域

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import storage from './storage';
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
@@ -14,6 +15,8 @@ export default {
     
   },
   mounted(){
+    
+    
     
 
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+  devServer: {//跨域
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://api-driver.marsview.cc/api/mall/',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
+        }
+      }
+    }
+  },
   transpileDependencies: true,
   lintOnSave: false
 })
