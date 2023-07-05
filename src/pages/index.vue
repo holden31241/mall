46cf55e8d01373fc8fff315e74871492929c99b1
holden <zcy19998006@163.com>
2023-07-05 18:03:23 +0800

    ads

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,6 +7,7 @@
                         <li class="menu-item">
                             <a href="javascript:;">手机 电话卡</a>
                             <div class="children">
+                                <!-- v-for必须有key -->
                                 <ul v-for="(item, i) in menuList" v-bind:key="i">
                                     <li v-for="(sub, j) in item" v-bind:key="j">
                                         <a v-bind:href="sub ? '/#/product/' + sub.id : ''">
@@ -54,10 +55,19 @@
                 </swiper>
     
             </div>
-            <div class="ads-box"></div>
-            <div class="banner"></div>
-            <div class="product-box"></div>
+                <div class="ads-box">
+                    <a v-bind:href="'/#/product/' + item.id" v-for="(item, index) in adsList" v-bind:key="index">
+                        <img v-bind:src="item.img" alt="">
+                    </a>
+                </div>
+                <div class="banner">
+                    <a href="/#/product/30">
+                        <img src="/imgs/banner-1.png" alt="">
+                    </a>
+                </div>
+                <div class="product-box"></div>       
         </div>
+    
     <service-bar></service-bar>
         
     </div>
@@ -120,11 +130,26 @@ export default {
             
         ]
 
-       
+    let adsList=[
+            {
+                id: 33,
+                img: '/imgs/ads/ads-1.png'
+            }, {
+                id: 48,
+                img: '/imgs/ads/ads-2.jpg'
+            }, {
+                id: 45,
+                img: '/imgs/ads/ads-3.png'
+            }, {
+                id: 47,
+                img: '/imgs/ads/ads-4.jpg'
+            }
+        ]   
         return{
             
            slideList,
            menuList,
+           adsList,
            modules: [Navigation, Pagination]
            
         } 
@@ -141,6 +166,7 @@ export default {
 <style lang="scss">
 @import './../assets/scss/config.scss';
 @import './../assets/scss/mixin.scss';
+@import './../assets/scss/base.scss';
 .index {
     .swiper-box {
          .nav-menu{
@@ -245,6 +271,18 @@ export default {
 
         }
         
+    }
+    .ads-box{
+      @include flex();
+      margin-top:14px;
+      margin-bottom:31px;
+      a{
+        width:296px;
+        height:167px;
+      }
+    }
+    .banner{
+      margin-bottom:50px;
     }
 }
 </style>
